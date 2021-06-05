ed0f3e26f79ee90be91df32d9fbde5f55a44db4a
Akshat-Rana <akshat511716@gmail.com>
2021-06-05 11:27:05 +0530

    fixed few minor bugs in the hamburger menu

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 const hamburger = document.querySelector('#nav-content .nav-icon');
 const menu = document.querySelector('#nav-content .container')
 
@@ -12,18 +9,3 @@ hamburger.addEventListener('click', ()=>{
         menu.classList.add('show');
     }
 })
-||||||| 539cf4a
-=======
-const hamburger = document.querySelector('#nav-content .nav-icon');
-// hamburger.style.display = 'none';
->>>>>>> d0e9edd6791e8a5b343d1ed245c084092d90965a
-||||||| 539cf4a
-=======
-const hamburger = document.querySelector('#nav-content .nav-icon');
-// hamburger.style.display = 'none';
->>>>>>> d0e9edd6791e8a5b343d1ed245c084092d90965a
-||||||| 539cf4a
-=======
-const hamburger = document.querySelector('#nav-content .nav-icon');
-// hamburger.style.display = 'none';
->>>>>>> refs/remotes/origin/master
