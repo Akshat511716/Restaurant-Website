08a91df73ec1c9f8f8010829040e1f1e47e106e0
Akshat-Rana <akshat511716@gmail.com>
2021-06-05 11:17:01 +0530

    Added animation to hamburger menu

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const hamburger = document.querySelector('#nav-content .nav-icon');
-// hamburger.style.display = 'none';
+const menu = document.querySelector('#nav-content .container')
+
+hamburger.addEventListener('click', ()=>{
+    if(menu.classList.contains('show')){
+        menu.classList.remove('show');
+    }
+    else{
+        menu.classList.add('show');
+    }
+})
