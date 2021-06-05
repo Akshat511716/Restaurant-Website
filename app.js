108b7abd006a7a4bd58703271bf1f8a486dbf27d
Akshat-Rana <akshat511716@gmail.com>
2021-06-05 11:20:31 +0530

    basic changes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 const hamburger = document.querySelector('#nav-content .nav-icon');
 const menu = document.querySelector('#nav-content .container')
 
@@ -10,6 +11,11 @@ hamburger.addEventListener('click', ()=>{
         menu.classList.add('show');
     }
 })
+||||||| 539cf4a
+=======
+const hamburger = document.querySelector('#nav-content .nav-icon');
+// hamburger.style.display = 'none';
+>>>>>>> d0e9edd6791e8a5b343d1ed245c084092d90965a
 ||||||| 539cf4a
 =======
 const hamburger = document.querySelector('#nav-content .nav-icon');
